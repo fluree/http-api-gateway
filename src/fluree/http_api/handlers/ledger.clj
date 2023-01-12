832e586a9e9106d7a60fd64eb0be21b5dfa972a3
Brian Platz <bplatz@gmail.com>
2023-01-12 13:04:09 -0500

    Hack to get memory db reloading

--- src/fluree/http_api/handlers/ledger.clj
+++ src/fluree/http_api/handlers/ledger.clj
@@ -18,13 +18,19 @@
   (log/debug "New ledger default context:" default-context)
   (deref! (fluree/create conn name {:context default-context})))
 
+(def DB-ATOM (atom {}))
+
 (defn transact
   [{:keys [fluree/conn] {{:keys [action ledger txn defaultContext]} :body} :parameters}]
   (println "\n\nTransacting to" ledger ":" (pr-str txn))
-  (let [[ledger status] (if (deref! (fluree/exists? conn ledger))
+  (let [ledger-name ledger
+        in-memory? (get @DB-ATOM ledger)
+        [ledger status] (if (or in-memory?
+                                (deref! (fluree/exists? conn ledger)))
                           (do
                             (log/debug "transact - Ledger" ledger "exists; loading it")
-                            [(deref! (fluree/load conn ledger)) 200])
+                            [(or (get @DB-ATOM ledger)
+                                 (deref! (fluree/load conn ledger))) 200])
                           (if (= :new (keyword action))
                             (do
                               (log/debug "transact - Ledger" ledger "does not exist; creating it")
@@ -32,12 +38,14 @@
                             (throw (ex-info "Ledger does not exist" {:ledger ledger}))))
         address (:address ledger)
         ;; TODO: Add a transact! fn to f.d.json-ld.api that stages and commits in one step
-        db      (-> ledger
-                    fluree/db
+        db-before (or (get @DB-ATOM ledger)
+                      (fluree/db ledger))
+        db      (-> db-before
                     (fluree/stage txn {:js? true})
                     deref!
                     fluree/commit!
                     deref!)]
+    (swap! DB-ATOM assoc ledger-name db)
     {:status status
      :body   (-> db
                  (select-keys [:alias :t])
@@ -45,8 +53,10 @@
 
 (defn query
   [{:keys [fluree/conn] {{:keys [ledger query]} :body} :parameters}]
-  (let [db     (->> ledger (fluree/load conn) deref! fluree/db)
+  (let [db     (or
+                 (get @DB-ATOM ledger)
+                 (->> ledger (fluree/load conn) deref! fluree/db))
         query* (reduce-kv (fn [acc k v] (assoc acc (keyword k) v)) {} query)]
     (log/debug "query - Querying ledger" ledger "-" query*)
     {:status 200
-     :body   (deref! (fluree/query db query*))}))
+     :body   (deref! (fluree/query  db (assoc-in query* [:opts :js?] true)))}))
