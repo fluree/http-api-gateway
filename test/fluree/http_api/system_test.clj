f7253ca8b9ac47c9c52fb7f47372ffab109bc067
Wes Morgan <git@wesmorgan.me>
2023-05-08 12:24:27 -0600

    Update commit-details tests now that it works
    In here we just expect the various keys and do some simple predicate checks on their values

--- test/fluree/http_api/system_test.clj
+++ test/fluree/http_api/system_test.clj
@@ -40,6 +40,18 @@
 
 (use-fixtures :once run-test-server)
 
+(def commit-id-regex
+  (re-pattern "fluree:commit:sha256:[a-z2-7]{52,53}"))
+
+(def mem-addr-regex
+  (re-pattern "fluree:memory://[a-f0-9]{64}"))
+
+(def context-id-regex
+  (re-pattern "fluree:context:[a-f0-9]{64}"))
+
+(def db-id-regex
+  (re-pattern "fluree:db:sha256:[a-z2-7]{52,53}"))
+
 (defn api-url [endpoint]
   (str "http://localhost:" @api-port "/fluree/" (name endpoint)))
 
@@ -297,67 +309,120 @@
 
 (deftest ^:integration ^:json history-query-json-test
   (testing "basic JSON history query works"
-    (let [ledger-name  (create-rand-ledger "history-query-basic-test")
-          json-headers {"Content-Type" "application/json"
-                        "Accept"       "application/json"}
-          txn-req      {:body
-                        (json/write-value-as-string
-                         {"ledger" ledger-name
-                          "txn"    [{"id"      "ex:query-test"
-                                     "type"    "schema:Test"
-                                     "ex:name" "query-test"}]})
-                        :headers json-headers}
-          txn-res      (post :transact txn-req)
-          _            (assert (= 200 (:status txn-res)))
-          query-req    {:body
-                        (json/write-value-as-string
-                         {"ledger" ledger-name
-                          "query"  {"commit-details" true
-                                    "t"              {"at" "latest"}}})
-                        :headers json-headers}
-          query-res    (post :history query-req)]
+    (let [ledger-name   "history-query-json-test"
+          json-headers  {"Content-Type" "application/json"
+                         "Accept"       "application/json"}
+          txn-req       {:body
+                         (json/write-value-as-string
+                          {"ledger" ledger-name
+                           "txn"    [{"id"      "ex:query-test"
+                                      "type"    "schema:Test"
+                                      "ex:name" "query-test"}]})
+                         :headers json-headers}
+          txn-res       (post :create txn-req)
+          _             (assert (= 201 (:status txn-res)))
+          txn2-req      {:body
+                         (json/write-value-as-string
+                          {"ledger" ledger-name
+                           "txn"    [{"id"           "ex:query-test"
+                                      "ex:test-type" "integration"}]})
+                         :headers json-headers}
+          txn2-res      (post :transact txn2-req)
+          _             (assert (= 200 (:status txn2-res)))
+          query-req     {:body
+                         (json/write-value-as-string
+                          {"ledger" ledger-name
+                           "query"  {"commit-details" true
+                                     "t"              {"at" "latest"}}})
+                         :headers json-headers}
+          query-res     (post :history query-req)
+          query-results (-> query-res :body json/read-value)]
       (is (= 200 (:status query-res))
           (str "History query response was: " (pr-str query-res)))
-      (is (= [{"f:commit"
-               {"f:data"
-                {"f:assert"
-                 [{"ex:name"  "query-test"
-                   "id"       "ex:query-test"
-                   "rdf:type" ["schema:Test"]}]
-                 "f:retract" []}}}]
-             (-> query-res :body json/read-value))))))
+      (let [history-expectations
+            {["id"]                  #(re-matches commit-id-regex %)
+             ["f:address"]           #(re-matches mem-addr-regex %)
+             ["f:alias"]             #(= % ledger-name)
+             ["f:branch"]            #(= % "main")
+             ["f:defaultContext"]    #(re-matches context-id-regex (get % "id"))
+             ["f:previous"]          #(or (nil? %)
+                                          (re-matches commit-id-regex (get % "id")))
+             ["f:time"]              pos-int?
+             ["f:v"]                 nat-int?
+             ["f:data" "f:address"]  #(re-matches mem-addr-regex %)
+             ["f:data" "f:assert"]   #(and (vector? %) (every? map? %))
+             ["f:data" "f:retract"]  #(and (vector? %) (every? map? %))
+             ["f:data" "f:flakes"]   pos-int?
+             ["f:data" "f:size"]     pos-int?
+             ["f:data" "f:t"]        pos-int?
+             ["f:data" "f:previous"] #(or (nil? %)
+                                          (re-matches db-id-regex (get % "id")))}]
+        (is (every? #(every? (fn [[kp valid?]]
+                               (let [actual (get-in % kp)]
+                                 (or
+                                  (valid? actual)
+                                  (println "invalid:" (pr-str actual)))))
+                             history-expectations)
+                    (map #(get % "f:commit") query-results)))))))
+
 
 (deftest ^:integration ^:edn history-query-edn-test
   (testing "basic EDN history query works"
-    (let [ledger-name (create-rand-ledger "history-query-basic-test")
-          edn-headers {"Content-Type" "application/edn"
-                       "Accept"       "application/edn"}
-          txn-req     {:body
-                       (pr-str
-                        {:ledger ledger-name
-                         :txn    [{:id      :ex/query-test
-                                   :type    :schema/Test
-                                   :ex/name "query-test"}]})
-                       :headers edn-headers}
-          txn-res     (post :transact txn-req)
-          _           (assert (= 200 (:status txn-res)))
-          query-req   {:body
-                       (pr-str
-                        {:ledger ledger-name
-                         :query  {:commit-details true
-                                  :t              {:at :latest}}})
-                       :headers edn-headers}
-          query-res   (post :history query-req)]
+    (let [ledger-name   "history-query-edn-test"
+          edn-headers   {"Content-Type" "application/edn"
+                         "Accept"       "application/edn"}
+          txn-req       {:body
+                         (pr-str
+                          {:ledger ledger-name
+                           :txn    [{:id      :ex/query-test
+                                     :type    :schema/Test
+                                     :ex/name "query-test"}]})
+                         :headers edn-headers}
+          txn-res       (post :create txn-req)
+          _             (assert (= 201 (:status txn-res)))
+          txn2-req      {:body
+                         (pr-str
+                          {:ledger ledger-name
+                           :txn    [{:id           :ex/query-test
+                                     :ex/test-type "integration"}]})
+                         :headers edn-headers}
+          txn2-res      (post :transact txn2-req)
+          _             (assert (= 200 (:status txn2-res)))
+          query-req     {:body
+                         (pr-str
+                          {:ledger ledger-name
+                           :query  {:commit-details true
+                                    :t              {:from 1}}})
+                         :headers edn-headers}
+          query-res     (post :history query-req)
+          query-results (-> query-res :body edn/read-string)]
       (is (= 200 (:status query-res))
           (str "History query response was: " (pr-str query-res)))
-      (is (= [{:f/commit
-               {:f/data
-                {:f/assert
-                 [{:ex/name  "query-test"
-                   :id       :ex/query-test
-                   :rdf/type [:schema/Test]}]
-                 :f/retract []}}}]
-             (-> query-res :body edn/read-string))))))
+      (let [history-expectations
+            {[:id]                 #(re-matches commit-id-regex %)
+             [:f/address]          #(re-matches mem-addr-regex %)
+             [:f/alias]            #(= % ledger-name)
+             [:f/branch]           #(= % "main")
+             [:f/defaultContext]   #(re-matches context-id-regex (:id %))
+             [:f/previous]         #(or (nil? %)
+                                        (re-matches commit-id-regex (:id %)))
+             [:f/time]             pos-int?
+             [:f/v]                nat-int?
+             [:f/data :f/address]  #(re-matches mem-addr-regex %)
+             [:f/data :f/assert]   #(and (vector? %) (every? map? %))
+             [:f/data :f/retract]  #(and (vector? %) (every? map? %))
+             [:f/data :f/flakes]   pos-int?
+             [:f/data :f/size]     pos-int?
+             [:f/data :f/t]        pos-int?
+             [:f/data :f/previous] #(or (nil? %)
+                                        (re-matches db-id-regex (:id %)))}]
+        (is (every? #(every? (fn [[kp valid?]]
+                               (let [actual (get-in % kp)]
+                                 (or
+                                  (valid? actual)
+                                  (println "invalid:" (pr-str actual)))))
+                             history-expectations)
+                    (map :f/commit query-results)))))))
 
 (deftest ^:integration ^:json policy-opts-json-test
   (testing "policy-enforcing opts are correctly handled"
